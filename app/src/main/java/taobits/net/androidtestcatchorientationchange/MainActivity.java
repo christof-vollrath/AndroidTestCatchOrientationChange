477eca559295083742e4e63a309b68f45c6deb21
Christof Vollrath <christof@taobits.net>
2016-06-15 22:53:01 +0200

    Added button to recreate app with changed layout

--- app/src/main/java/taobits/net/androidtestcatchorientationchange/MainActivity.java
+++ app/src/main/java/taobits/net/androidtestcatchorientationchange/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,19 +13,34 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("taobits2", "onCreate");
+        Log.i("taobits", "onCreate");
+        addListenerOnButton();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i("taobits2", "onResume");
+        Log.i("taobits", "onResume");
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        Log.i("taobits2", "onConfigurationChanged: " + newConfig);
+        Log.i("taobits", "onConfigurationChanged: " + newConfig);
+    }
+
+    public void addListenerOnButton() {
+        Button button = (Button) findViewById(R.id.redraw);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("taobits", "onClick");
+                recreate();  // Exists only since API Level 11 Honeycomb
+            }
+        });
+
+
     }
 
 }
